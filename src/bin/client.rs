42ed3781c030d2bf480667e9be2565d23ba388a6
jakobrs <jakobrs100@gmail.com>
2022-09-04 10:31:30 +0200

    Just don't attempt to call finish() on send_stream, at all

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -89,8 +89,5 @@ async fn handle_client(mut client: TcpStream, conn: quinn::Connection) -> Result
     let stats = tokio::io::copy_bidirectional(&mut client, &mut quinn_bi_stream).await?;
     log::info!("Stats: {stats:?}");
 
-    log::info!("Closing connection with client, and stream");
-    quinn_bi_stream.send_stream.finish().await?;
-
     return Ok(());
 }
